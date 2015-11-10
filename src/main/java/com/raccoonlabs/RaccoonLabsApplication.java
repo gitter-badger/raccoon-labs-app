b6c56654321ba261fc0815a3ecb6f45aecfde25b
sergiikushnir <CapGrey3333>
2015-11-10 17:14:32 +0200

    Initial project commit

---
+++ src/main/java/com/raccoonlabs/RaccoonLabsApplication.java
@@ -0,0 +1,12 @@
+package com.raccoonlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaccoonLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaccoonLabsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/raccoonlabs/config/DatasoureConfig.java
@@ -0,0 +1,4 @@
+package com.raccoonlabs.config;
+
+public class DatasoureConfig {
+}
